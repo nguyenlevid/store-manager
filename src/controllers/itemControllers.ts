8b93facdde8a4be9418196c4df8a4ebb0517dfad
kai <nguyenlenguyen.1042006@gmail.com>
2025-12-26 21:35:37 -0500

    feat: itemFunctions

--- src/controllers/itemControllers.ts
+++ src/controllers/itemControllers.ts
@@ -3,11 +3,44 @@ import { createFactory } from 'hono/factory';
 
 const factory = createFactory();
 
-// POST
+// POST 1 ITEM
 export const createItem = factory.createHandlers(async (c) => {
   const body = await getBody(c);
 
-  return c.json({ message: `Body received: ${JSON.stringify(body)}` }, 200);
+  return c.json(
+    { createType: 'item', message: `Body received: ${JSON.stringify(body)}` },
+    200,
+  );
+});
+
+// POST ITEMS
+export const createItems = factory.createHandlers(async (c) => {
+  const body = await getBody(c);
+
+  return c.json(
+    { createType: 'items', message: `Body received: ${JSON.stringify(body)}` },
+    200,
+  );
+});
+
+// PUT (Update)
+export const updateItem = factory.createHandlers(async (c) => {
+  const body = await getBody(c);
+  const params = await getParams(c);
+
+  return c.json(
+    {
+      message: `Body received: ${JSON.stringify(body)} Params received: ${JSON.stringify(params)}`,
+    },
+    200,
+  );
+});
+
+// DELETE
+export const deleteItem = factory.createHandlers(async (c) => {
+  const params = await getParams(c);
+
+  return c.json({ message: `Successfully deleted` }, 200);
 });
 
 // GET by ID
@@ -18,6 +51,18 @@ export const getItemById = factory.createHandlers(async (c) => {
   return c.json(
     {
       message: `Queries received: ${JSON.stringify(queries)}, Params received: ${JSON.stringify(params)}`,
+    },
+    200,
+  );
+});
+
+// GET ALL
+export const getItems = factory.createHandlers(async (c) => {
+  const queries = await getQuery(c);
+
+  return c.json(
+    {
+      message: `Queries received: ${JSON.stringify(queries)}`,
     },
     200,
   );
--- src/routes/itemRoutes.ts
+++ src/routes/itemRoutes.ts
@@ -1,10 +1,21 @@
-import { createItem, getItemById } from '@/controllers/itemControllers';
+import {
+  createItem,
+  createItems,
+  getItems,
+  deleteItem,
+  getItemById,
+  updateItem,
+} from '@/controllers/itemControllers';
 import { Hono } from 'hono';
 
 const itemRoutes = new Hono();
 
 itemRoutes
   .post('/', ...createItem) // POST /api/item
-  .get('/:id', ...getItemById); // GET /api/item/:id
+  .get('/', ...getItems) // GET /api/item/
+  .get('/:id', ...getItemById) // GET /api/item/:id
+  .post('/items', ...createItems) // POST /api/item/items
+  .put('/:id', ...updateItem) // PUT /api/item/:id
+  .delete('/:id', ...deleteItem); // DELETE /api/item/:id
 
 export default itemRoutes;
